3f66d2c731c54f2c9556de10015d371f050ff07e
Kemzi <kemzi9585@gmail.com>
2021-10-14 21:55:43 +0300

    Pushing code

--- private.js
+++
@@ -1,7 +0,0 @@
-const token = '1965270611:AAHJIJgqCPADHA2OnZ7ne826ExddDWfLYbg'
-const mongoURI = 'mongodb+srv://kemzi:DotaHey33@cluster0.r9zb4.mongodb.net/telegrambot?retryWrites=true&w=majority'
-
-module.exports = {
-	mongoURI,
-	token
-}
