0d3d0ba028094132fdeb2bc80533356cf73cb1b7
Kemzi <kemzi9585@gmail.com>
2021-10-14 21:50:12 +0300

    Some changes

--- private.js
+++ private.js
@@ -1,5 +1,5 @@
 const token = '1965270611:AAHJIJgqCPADHA2OnZ7ne826ExddDWfLYbg'
-const mongoURI = 'mongodb+srv://kemzi:DotaHey33@cluster0.r9zb4.mongodb.net/telegabot?retryWrites=true&w=majority'
+const mongoURI = 'mongodb+srv://kemzi:DotaHey33@cluster0.r9zb4.mongodb.net/telegrambot?retryWrites=true&w=majority'
 
 module.exports = {
 	mongoURI,
