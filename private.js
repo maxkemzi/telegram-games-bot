7e53494004380dc1d2b9ca069c9420d2179451cc
Kemzi <kemzi9585@gmail.com>
2021-10-14 21:35:32 +0300

    Token change

--- private.js
+++ private.js
@@ -1,4 +1,4 @@
-const token = '1965582393:AAEzzhmBjgwvfpLJbw8pu6Z5y3BUYNXFBGc'
+const token = '1965270611:AAHJIJgqCPADHA2OnZ7ne826ExddDWfLYbg'
 const mongoURI = 'mongodb+srv://kemzi:DotaHey33@cluster0.r9zb4.mongodb.net/telegabot?retryWrites=true&w=majority'
 
 module.exports = {
